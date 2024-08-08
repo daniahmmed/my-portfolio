f9107ea4442ff783351415fe43b953aea9daa408
daniahmmed <daniahmmed1@gmail.com>
2024-08-09 02:43:38 +0530

    Contact and Page Navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,3 +85,43 @@ const filterFunc = function (selectedValue) {
         }
     }
 }
+
+// contact form variable
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input fields
+for (let i = 0; i < formInputs.length; i++) {
+    formInputs[i].addEventListener("input", function () {
+
+        // check form validation
+        if (form.checkValidity()) {
+            formBtn.removeAttribute("disabled");
+        } else {
+            formBtn.setAttribute("disabled", "")
+        }
+
+    });
+}
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener("click", function () {
+        
+        for (let i = 0; i < pages.length; i++) {
+            if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+                pages[i].classList.add("active");
+                navigationLinks[i].classList.add("active");
+                window.scrollTo(0, 0);             
+            } else {
+                pages[i].classList.remove("active");
+                navigationLinks[i].classList.remove("active");
+            }
+        }
+    });
+}
